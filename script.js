a022d3300915658943e83b78643822ba412067a1
Petros-Petrosyan <petros.petrosyan.1996@gmail.com>
2023-06-27 01:18:22 +0400

    Update

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const canvas = document.querySelector("canvas");
-const btn = document.querySelector("button");
-const context = canvas.getContext("2d");
+const CANVAS = document.querySelector("canvas");
+const BUTTON = document.querySelector("button");
+const CONTEXT = CANVAS.getContext("2d");
 
 const data = {
   balls: [],
+};
+
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
 const update = () => {
@@ -13,7 +17,7 @@ const update = () => {
 };
 
 const draw = () => {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
   data.balls.forEach((ball) => {
     ball.draw();
   });
@@ -28,39 +32,38 @@ const loop = () => {
 loop();
 
 function Ball() {
-  this.r = random(5, 40);
-  this.x = random(this.r, canvas.width - this.r);
-  this.y = random(this.r, canvas.height - this.r);
+  const radius = getRandomNumber(5, 40);
+  let x = getRandomNumber(radius, CANVAS.width - radius);
+  let y = getRandomNumber(radius, CANVAS.height - radius);
 
-  this.xDelta = random(-5, 5);
-  this.yDelta = random(-5, 5);
-  this.color = `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+  let xDelta = getRandomNumber(-5, 5);
+  let yDelta = getRandomNumber(-5, 5);
+  const color = `rgb(${getRandomNumber(0, 255)},${getRandomNumber(
+    0,
+    255
+  )},${getRandomNumber(0, 255)})`;
 
   this.update = function () {
-    if (this.x + this.r > canvas.width || this.x - this.r < 0) {
-      this.xDelta *= -1;
+    if (x + radius > CANVAS.width || x - radius < 0) {
+      xDelta *= -1;
     }
-    if (this.y + this.r > canvas.height || this.y - this.r < 0) {
-      this.yDelta *= -1;
+    if (y + radius > CANVAS.height || y - radius < 0) {
+      yDelta *= -1;
     }
 
-    this.x += this.xDelta;
-    this.y += this.yDelta;
+    x += xDelta;
+    y += yDelta;
   };
 
   this.draw = function () {
-    context.fillStyle = this.color;
-    context.beginPath();
-    context.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-    context.fill();
+    CONTEXT.fillStyle = color;
+    CONTEXT.beginPath();
+    CONTEXT.arc(x, y, radius, 0, 2 * Math.PI);
+    CONTEXT.fill();
   };
 }
 
-btn.addEventListener("click", function () {
+BUTTON.addEventListener("click", function () {
   const ball = new Ball();
   data.balls.push(ball);
 });
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
